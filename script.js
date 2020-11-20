8759d77a534423bd2b86c8728117dd760c335b3a
amcmilne <amcmilne@gmail.com>
2020-11-20 11:38:02 -0500

    adding js file

---
+++ script.js
@@ -0,0 +1,21 @@
+// GIVEN a weather dashboard with form inputs
+
+
+// I search for a city //SEARCH FEATURE
+
+
+// I am presented with current and future conditions for that city //retrieve info from API
+
+// that city is added to the search history
+
+// I view current weather conditions for that city: the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index //retrieve this data from API, load to the html
+
+// I view the UV index I am presented with a color that indicates whether the conditions are favorable, moderate, or severe //ASSIGN COLOR BASED ON UV INDEX FROM API
+
+// I view future weather conditions for that city I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity //PULL FROM API AND LOAD IN HTML
+
+// I click on a city in the search history //PULL FROM LOCAL STORAGE
+
+// I am again presented with current and future conditions for that city
+
+// I open the weather dashboard I am presented with the last searched city forecast //PULL FROM LOCAL STORAGE
